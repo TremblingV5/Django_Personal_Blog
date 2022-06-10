69a08a66ddffa4f822f02d1751a76d90e9c9632f
TremblingV5 <Administrator@XinZF>
2022-06-10 18:28:39 +0800

    暂时屏蔽mermaid

--- Code/utils/parser.py
+++ Code/utils/parser.py
@@ -2,9 +2,9 @@ import markdown
 
 def parse_md(text):
     return markdown.markdown(text.replace("\r\n", ' \n'), extensions=[
+        # 'md_mermaid',
         'markdown.extensions.extra',
         'markdown.extensions.codehilite',
         'markdown.extensions.toc',
         'mdx_math',
-        'md_mermaid'
     ])
