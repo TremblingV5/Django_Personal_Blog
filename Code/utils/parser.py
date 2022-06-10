99e4f362ea8459262e0e69c7bd318fbb79842767
TremblingV5 <Administrator@XinZF>
2022-06-10 19:31:42 +0800

    解决mermaid过滤掉中文的问题

--- Code/utils/parser.py
+++ Code/utils/parser.py
@@ -2,7 +2,7 @@ import markdown
 
 def parse_md(text):
     return markdown.markdown(text.replace("\r\n", ' \n'), extensions=[
-        # 'md_mermaid',
+        'utils.mdeditor_extensions.mermaid',
         'markdown.extensions.extra',
         'markdown.extensions.codehilite',
         'markdown.extensions.toc',
